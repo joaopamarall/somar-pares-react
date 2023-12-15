beedea77ec4c06b1e3cc22ee26e98789dfcb81c1
joaopamarall <joaoamaral56@hotmail.com>
2023-12-15 13:50:40 -0300

    refactor: refatorar código para melhor leitura de lógica.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 import './App.css'
 
-function SomaNumerosPares({ numerosInteiros }) {
-
-  const numerosPares = numerosInteiros.flat().filter((element) => element % 2 === 0);
-
-  const soma = numerosPares.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
-  return (
-    <div>
-      <p>A soma dos números pares é: {soma}</p>
-    </div>
-  );
-}
-
 function App() {
-  const matrizNumerosInteiros = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]];
+  const matrizNumerosInteiros = [[12, 20, 35, 4], [58, 60, 7, 81], [97, 100, 115, 120], [135, 145, 155, 160]];
+  const numerosPares = matrizNumerosInteiros.flat().filter((element) => element % 2 === 0);
+  const soma = numerosPares.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
 
   return (
     <>
-    <h1>Teste de Soma de Números Pares em React</h1>
-      <SomaNumerosPares numerosInteiros={matrizNumerosInteiros} />
+      <h1>Teste de Soma de Números Pares em React</h1>
+      <div>
+        <p>A soma dos números pares é: {soma}</p>
+      </div>
     </>
   )
 }
